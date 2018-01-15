fca8a47f9717300e083d322d23dbff2d6d1e2196
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2018-01-15 21:03:09 +0300

    CS fixes

--- src/js/router.js
+++ src/js/router.js
@@ -12,38 +12,38 @@ export default class SelamiRouter {
   }
 
   dispatch () {
-    const controller = this.pascalCase(this.controllerInfo.shift()) + "Service"
-    const method = this.camelCase(this.controllerInfo.shift())
-    return {controller, method, "args": this.controllerInfo, }
+    const controller = this.pascalCase(this.controllerInfo.shift()) + "Service";
+    const method = this.camelCase(this.controllerInfo.shift());
+    return {controller, method, "args": this.controllerInfo}
   }
 
   getControllerInfo () {
-    let items = this.trimRequestedPath(this.requestedPath).split("/")
+    let items = this.trimRequestedPath(this.requestedPath).split("/");
     if (typeof items[1] === "undefined" || items[1] === "") {
-      items[1] = "default"
+      items[1] = "default";
     }
-    return items
+    return items;
   }
 
   trimRequestedPath (str) {
-    return str.replace(/^\/+/g, "")
+    return str.replace(/^\/+/g, "");
   }
 
   pascalCase (str) {
-    const items = str.split("-")
+    const items = str.split("-");
     for (let index = 0; index < items.length; index++) {
-      let item = items[index]
-      items[index] = item[0].toUpperCase() + item.slice(1).toLowerCase()
+      let item = items[index];
+      items[index] = item[0].toUpperCase() + item.slice(1).toLowerCase();
     }
-    return items.join("").replace(/\W+/g, "")
+    return items.join("").replace(/\W+/g, "");
   }
 
   camelCase (str) {
-    const items = str.split("-")
+    const items = str.split("-");
     for (let index = 0; index < items.length; index++) {
-      let item = items[index]
-      items[index] = (index === 0 ? item[0].toLowerCase() : item[0].toUpperCase()) + item.slice(1).toLowerCase()
+      let item = items[index];
+      items[index] = (index === 0 ? item[0].toLowerCase() : item[0].toUpperCase()) + item.slice(1).toLowerCase();
     }
-    return items.join("").replace(/\W+/g, "")
+    return items.join("").replace(/\W+/g, "");
   }
 }
