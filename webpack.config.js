488d7c4a94d7df7f06af0b72d2064f0561d5962c
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2018-01-17 21:25:22 +0300

    watch option removed from the config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,6 @@ module.exports = {
     filename: "assets/js/bundle.[hash].js",
     path: path.resolve(__dirname, "dist")
   },
-  watch: true,
   resolve: {
     alias: {
       jquery: "jquery/src/jquery",
